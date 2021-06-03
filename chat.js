acf943ce8654c764d88cf724000be56a3b099016
Sarah Franke <sarah.franke@hs-furtwangen.de>
2021-06-03 11:46:08 +0200

    next commit

---
+++ chat.js
@@ -0,0 +1,18 @@
+"use strict";
+var soundMotion;
+(function (soundMotion) {
+    window.addEventListener("load", handleLoad);
+    let inputField;
+    let loginBtn;
+    function handleLoad(_event) {
+        inputField = document.querySelector("#inputField");
+        loginBtn = document.querySelector("#loginBtn");
+        loginBtn.addEventListener("click", getUsername);
+    }
+    function getUsername(_event) {
+        let username = inputField.value;
+        console.log(username);
+        alert("dein Nutzername ist:" + username);
+    }
+})(soundMotion || (soundMotion = {})); //Ende Namespance
+//# sourceMappingURL=chat.js.map

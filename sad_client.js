63c3da4ec3dc155377f9c41228616fd0e8a61b7e
Sarah Franke <sarah.franke@hs-furtwangen.de>
2021-07-08 19:18:06 +0200

    client sad implemented

--- sad_client.js
+++ sad_client.js
@@ -1,41 +1,37 @@
 "use strict";
 var soundMotion;
 (function (soundMotion) {
-    // const socket: WebSocket = new WebSocket("ws://localhost:5000");
     const socket = new WebSocket("wss://soundmotion.herokuapp.com/");
     window.addEventListener("load", handleLoad);
     let audio = new Audio();
-    let love1;
-    let love2;
-    let love3;
-    let loveSend1;
-    let loveSend2;
-    let loveSend3;
+    let sad1;
+    let sad2;
+    let sad3;
+    let sadSend1;
+    let sadSend2;
+    let sadSend3;
     function handleLoad(_event) {
-        // Category Love ******
-        love1 = document.querySelector("#love1");
-        love2 = document.querySelector("#love2");
-        love3 = document.querySelector("#love3");
-        loveSend1 = document.querySelector("#love1 button");
-        loveSend2 = document.querySelector("#love2 button");
-        loveSend3 = document.querySelector("#love3 button");
-        //Play Sound
-        love1.addEventListener("click", function () { play("./assets/sounds/love/hey_im_in_love.mp3"); });
-        love2.addEventListener("click", function () { play("./assets/sounds/love/I_want_ur_stupid_love.mp3"); });
-        love3.addEventListener("click", function () { play("./assets/sounds/love/keep_on_falling_in_love.mp3"); });
-        // Send Sound 
-        loveSend1.addEventListener("click", function (e) { console.log("sending song1"); sendSound("./assets/sounds/love/hey_im_in_love.mp3"); e.stopImmediatePropagation(); });
-        loveSend2.addEventListener("click", function (e) { sendSound("./assets/sounds/love/I_want_ur_stupid_love.mp3"); e.stopImmediatePropagation(); });
-        loveSend3.addEventListener("click", function (e) { sendSound("./assets/sounds/love/keep_on_falling_in_love.mp3"); e.stopImmediatePropagation(); });
+        sad1 = document.querySelector("#sad1");
+        sad2 = document.querySelector("#sad2");
+        sad3 = document.querySelector("#sad3");
+        sadSend1 = document.querySelector("#sad1 button");
+        sadSend2 = document.querySelector("#sad2 button");
+        sadSend3 = document.querySelector("#sad3 button");
+        sad1.addEventListener("click", function () { play("./assets/sounds/sad/just_a_sad_song_V2.mp3"); });
+        sad2.addEventListener("click", function () { play("./assets/sounds/sad/sad_situation.mp3"); });
+        sad3.addEventListener("click", function () { play("./assets/sounds/sad/nobody_knows.mp3"); });
+        sadSend1.addEventListener("click", function (e) { console.log("sending song1"); sendSound("./assets/sounds/sad/just_a_sad_song_V2.mp3"); e.stopImmediatePropagation(); });
+        sadSend2.addEventListener("click", function (e) { sendSound("./assets/sounds/sad/sad_situation.mp3"); e.stopImmediatePropagation(); });
+        sadSend3.addEventListener("click", function (e) { sendSound("./assets/sounds/sad/nobody_knows.mp3"); e.stopImmediatePropagation(); });
     }
-    let messageList;
-    // get div element
-    // const messageListDiv: HTMLDivElement = <HTMLInputElement>document.getElementById("chatArea");
     function play(_soundpiece) {
         audio.pause();
         audio.src = _soundpiece;
         audio.play();
     }
+    let messageList; //= null; ?????
+    // get div element
+    // const messageListDiv: HTMLDivElement = <HTMLInputElement>document.getElementById("chatArea");
     ///WENN DER CLIENT EINE MASSAGE EMPFÄNGT
     socket.addEventListener("message", (event) => {
         const carrier = JSON.parse(event.data);
@@ -77,5 +73,5 @@ var soundMotion;
     socket.addEventListener("open", () => {
         console.log("We are connected");
     });
-})(soundMotion || (soundMotion = {})); //Ende Namespance
-//# sourceMappingURL=client.js.map
+})(soundMotion || (soundMotion = {}));
+//# sourceMappingURL=sad_client.js.map
---
+++ sad_client.ts
@@ -0,0 +1,117 @@
+namespace soundMotion {
+    const socket: WebSocket = new WebSocket("wss://soundmotion.herokuapp.com/");
+
+
+    window.addEventListener("load", handleLoad);
+    let audio: HTMLAudioElement = new Audio();
+    let sad1: HTMLDivElement;
+    let sad2: HTMLDivElement;
+    let sad3: HTMLDivElement;
+    let sadSend1: HTMLButtonElement;
+    let sadSend2: HTMLButtonElement;
+    let sadSend3: HTMLButtonElement;
+
+
+    function handleLoad(_event: Event): void {
+
+        sad1 = <HTMLDivElement>document.querySelector("#sad1");
+        sad2 = <HTMLDivElement>document.querySelector("#sad2");
+        sad3 = <HTMLDivElement>document.querySelector("#sad3");
+        sadSend1 = <HTMLButtonElement>document.querySelector("#sad1 button");
+        sadSend2 = <HTMLButtonElement>document.querySelector("#sad2 button");
+        sadSend3 = <HTMLButtonElement>document.querySelector("#sad3 button");
+
+        sad1.addEventListener("click", function () { play("./assets/sounds/sad/just_a_sad_song_V2.mp3") });
+        sad2.addEventListener("click", function () { play("./assets/sounds/sad/sad_situation.mp3") });
+        sad3.addEventListener("click", function () { play("./assets/sounds/sad/nobody_knows.mp3") });
+
+        sadSend1.addEventListener("click", function (e) { console.log("sending song1"); sendSound("./assets/sounds/sad/just_a_sad_song_V2.mp3"); e.stopImmediatePropagation(); });
+        sadSend2.addEventListener("click", function (e) { sendSound("./assets/sounds/sad/sad_situation.mp3"); e.stopImmediatePropagation(); });
+        sadSend3.addEventListener("click", function (e) { sendSound("./assets/sounds/sad/nobody_knows.mp3"); e.stopImmediatePropagation(); });
+
+
+    }
+
+    function play(_soundpiece: string): void {
+        audio.pause();
+        audio.src = _soundpiece;
+        audio.play();
+    }
+
+    // carrier message interface
+    interface CarrierMessage {
+        selector: string;
+        data?: string;
+    }
+    // client init message interface
+    interface InitMessage {
+        //  userNames: string;
+        messages: TextMessage[];
+    }
+    // client text message interface
+    interface TextMessage {
+        // uerNames: string;
+        text: string;
+    }
+    let messageList: TextMessage[]; //= null; ?????
+
+    // get div element
+    // const messageListDiv: HTMLDivElement = <HTMLInputElement>document.getElementById("chatArea");
+
+
+    ///WENN DER CLIENT EINE MASSAGE EMPFÄNGT
+    socket.addEventListener("message", (event) => {
+        const carrier: CarrierMessage = <CarrierMessage>JSON.parse(event.data);
+        const selector: string = carrier.selector;
+        const data: string = <string>carrier.data;
+
+        switch (selector) {
+            case "init": {
+                const initMessage: InitMessage = <InitMessage>JSON.parse(data);
+
+                // store userNames and message list
+                // userNames = initMessage.userNames;
+                messageList = initMessage.messages;
+                // displayListUserNames();
+                break;
+            }
+
+            case "text-message": {
+                const textMessage: TextMessage = <TextMessage>JSON.parse(<string>data);
+                messageList.push(textMessage); // add message to message list
+                audio.src = textMessage.text;
+                audio.play();
+                console.log(textMessage.text);
+
+                // displayListUserNames();
+                break;
+            }
+        }
+
+    });
+
+    // WENN DER CLIENT EINE MASSAGE VERSCHICKT
+
+    function sendSound(_soundpeace: string): void {
+        // window.location.href = "chat.html";
+        let soundToSend: string = _soundpeace;
+
+        const message: TextMessage = {
+            text: soundToSend
+        };
+
+        const textCarrier: CarrierMessage = {
+            selector: "text-message",
+            data: JSON.stringify(message)
+        };
+        socket.send(JSON.stringify(textCarrier));
+    }
+
+    socket.addEventListener("open", () => {
+        console.log("We are connected");
+    });
+
+
+
+
+}
